61cbb67f3586a8f7286da4c3cd12b7c246dc2d08
gogowonji <spacedau@gmail.com>
2021-08-06 23:19:27 +0900

    made deal button function

--- war-challenge/ContentView.swift
+++ war-challenge/ContentView.swift
@@ -8,8 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    
+    @State var playerCard = "card5"
+    @State var cpuCard = "card9"
+    @State var playerScore = 0
+    @State var cpuScore = 0
+    
+//    @State mean
+//    뷰가 접근 가능하도록 해줌
+//    it's a piece of data that the view code relies upon to display the UI.
+//    1.Change the data
+//    2. UI automatically updated
+
+    
     var body: some View {
+        
         //safe area (활용 가능한 부분) <-> ignoring area
+                
+        
         ZStack{
             Image("background").edgesIgnoringSafeArea(.all)
             
@@ -22,18 +39,32 @@ struct ContentView: View {
                 
                 HStack{
                     Spacer()
-                    Image("card3")
+                    Image(playerCard)
                     
                     Spacer()
 
-                    Image("card4")
+                    Image(cpuCard)
                     
                     Spacer()
                 }
                 Spacer()
                 
                 //first Button test!
-                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                Button(action: {
+                    //Gernate a random card between 2 and 14
+                    let playerRand = Int.random(in: 2...14)
+                    let cpuRand = Int.random(in: 2...14)
+                    
+                    //Update the cards
+                    playerCard = "card" + String(playerRand)
+                    cpuCard = "card" + String(cpuRand)
+                    
+                    
+                    //Update the scores
+                    playerScore += 1
+                    cpuScore += 1
+                    
+                }, label: {
                     Image("dealbutton")
                 })
                 
@@ -46,7 +77,7 @@ struct ContentView: View {
                         Text("Player")
                             .font(.headline)
                             .padding(.bottom, 10.0)
-                        Text("0")
+                        Text(String(playerScore))
                             .font(.largeTitle)
                     }.padding()
                     Spacer()
@@ -54,7 +85,7 @@ struct ContentView: View {
                         Text("CPU")
                             .font(.headline)
                             .padding(.bottom, 10.0)
-                        Text("0")
+                        Text(String(cpuScore))
                             .font(.largeTitle)
                     }.padding()
                     Spacer()
